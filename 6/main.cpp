ee0686a776293a9052d13356a1ec9afb47df57d7
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-10 23:03:23 +0300

    6

--- 6/main.cpp
+++ 6/main.cpp
@@ -1,14 +1,97 @@
-//
-//  main.cpp
-//  6
-//
-//  Created by Никита Кузнецов on 10.12.2020.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+//1 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a, b;
+//    cin >> a >> b;
+//    a+=b;
+//    b=max(a,b)-min(a,b);
+//    a=max(a,b)-min(a,b);
+//    cout << a << " " << b;
+//}
+//2 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a, b, c;
+//    cin >> a >> b >> c;
+//    double temp_a=a;
+//    a = c;
+//    c = b;
+//    b = temp_a;
+//    cout << a << " " << b << " " << c;
+//}
+//3 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a, b, c;
+//    cin >> a >> b >> c;
+//    double temp_a=a;
+//    a = b;
+//    b = c;
+//    c = temp_a;
+//    cout << a << " " << b << " " << c;
+//}
+//4 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//double F_4(double a){
+//    double y;
+//    y=3*(a*a*a*a*a*a)-6*(a*a)-7;
+//    return y;
+//}
+//int main(){
+//double x;
+//cin >> x;
+//cout << F_4(x);
+//}
+//5 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//double F_5(double a){
+//    double y;
+//    y=4*(a-3)*(a-3)*(a-3)*(a-3)*(a-3)*(a-3)-7*(a-3)*(a-3)*(a-3)+2;
+//    return y;
+//}
+//int main(){
+//double x;
+//cin >> x;
+//cout << F_5(x);
+//}
+//6 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a;
+//    cin >> a;
+//    double tempa;
+//    a=a*a;
+//    a=a*a;
+//    tempa=a;
+//    a=tempa*a;
+//    cout << a;
+//}
+//7 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a;
+//    double b, c;
+//    cin >> a;
+//    b = a; //
+//    a = a * a;//2 st
+//    a = a * a;//4 st
+//    a = a * b;//5 st
+//    c = a; //
+//    a = a * a;//10 st
+//    a = a * c; //15 st
+//    cout << a;
+//}
